b9251ba1ee90fb2f020395b0018bf0ef80ea007e
nuch0208 <nutchanet2887@gmail.com>
2024-08-28 08:47:14 +0700

    update

--- todo.js
+++ todo.js
@@ -1,16 +1,16 @@
-const taskInput = document.querySelector('#task-input')
+const taskInput = document.querySelector('#task-input') //querySelector คือคำสั่งสำหรับเข้าถึง element #task-input ตัวแรกที่พบและคืนค่า
 const taskList = document.querySelector('#task-list')
 const form = document.querySelector('form')
-let todos = JSON.parse( sessionStorage.getItem('todos')) || []
+let todos = JSON.parse( sessionStorage.getItem('todos')) || [] //JSON.parse() สำหรับแปลง string เป็น object หรือ แปลง object เป็น array
 
 
 function createListElement(task){
-    const taskItem = document.createElement('li')
+    const taskItem = document.createElement('li')  //สร้าง element แบบ list 
     taskItem.innerText = task
-    taskItem.classList.add('text-gray-700')
+    taskItem.classList.add('text-gray-700') 
     taskItem.classList.add('text-2xl')
 
-    taskItem.addEventListener('click', () => {
+    taskItem.addEventListener('click', () => {   //event click
         taskItem.classList.toggle('line-through')
         taskItem.classList.toggle('text-gray-400')
     })
@@ -33,7 +33,7 @@ form.addEventListener('submit', e => {
 
 function storeTodos(item){
     todos.push(itme)
-    sessionStorage.setItem('todos', JSON.stringify(todos))
+    sessionStorage.setItem('todos', JSON.stringify(todos)) //JSON.stringify()เป็นการแปลงข้อมมูลจาก JavaScript Object ให้อยู่ในรูปของ JSON String เมื่อทำการเรียกใช้งานฟังก์ชันแล้ว ก็จะได้ค่าที่เป็น string 
     
 }
 
